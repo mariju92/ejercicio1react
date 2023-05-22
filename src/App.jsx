079e827804db346397d1e16f8fb30bbfce84ee73
Marianjuavi <mariana.ju92@gmail.com>
2023-05-22 08:12:45 -0300

    Se realiza el ejercicio1 del tp de react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Saludo from './components/Saludo'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+
+  return (
+    <>
+<Saludo/>
+ </>
+  )
+}
+
+export default App
---
+++ src/components/Saludo.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Saludo = () => {
+  return (
+    <div>
+      <h1 className="text-center my-5">Hello world!</h1>
+    </div>
+  );
+};
+
+export default Saludo;
